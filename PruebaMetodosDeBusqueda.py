0f384be185dc834b5fece95d3cfd4b1755772b3a
Guadalupe Vazquez <guvadlt@gmail.com>
2018-12-03 08:45:19 -0600

    Se agrego el menu para BusquedaBinaria

--- PruebaMetodosDeBusqueda.py
+++ PruebaMetodosDeBusqueda.py
@@ -5,6 +5,19 @@ Created on 29/11/2018
 '''
 import random
 import copy
+
+def ordenamientoPorInsercion(numeros):
+    for i in range(1,len(numeros)):
+        aux=numeros[i]
+        j=i-1
+        while j>=0:
+            if(aux<numeros[j]):
+                numeros[j+1]=numeros[j]
+                numeros[j]=aux
+                j=j-1
+            else:
+                break
+
 def busquedaSecuencial(unaLista,datoBuscar):
     pos=0
     encontrado=False
@@ -16,13 +29,12 @@ def busquedaSecuencial(unaLista,datoBuscar):
             pos=pos+1
     return encontrado
 
-def busquedaBinaria(self,unaLista,elemento):
+def busquedaBinaria(unaLista,elemento):
     primero=0
     ultimo=len(unaLista)-1
     while(primero<=ultimo):
         centro=int ((primero+ultimo)/2)
-        valorCentro=unaLista(centro)
-        print("Comparando "+str(elemento)+" con "+str(unaLista[centro]))
+        valorCentro=unaLista[centro]
         
         if(elemento==valorCentro):
             return centro
@@ -38,12 +50,19 @@ menu=0
 arregloDesordenado = [0]  * 1000
 for i in range(1000):
     arregloDesordenado[i] = random.randint(0, 100)
-while(menu!=2):
+while(menu!=3):
     print ("_________MENU__________")
     print ("1. Busqueda Secuencial")
-    menu=int(input("2.Salir"))
+    print ("2. Busqueda Binaria")
+    menu=int(input("3.Salir"))
     
     if menu==1:
         datoBuscar=int (input("Ingresa dato a buscar"))
         print (busquedaSecuencial(copy.copy(arregloDesordenado), datoBuscar))
-    
+    if menu==2:
+        datoBuscar=int (input("Ingresa dato a buscar"))
+        arr=copy.copy(arregloDesordenado)
+        ordenamientoPorInsercion(arr)
+        a=busquedaBinaria(arr,datoBuscar)
+        if(a!=-1):
+            print("Dato encontrado!")  
