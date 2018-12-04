b7b12cf55b6eb0ce9d45e215c8e441e00fa9e22b
Guadalupe Vazquez <guvadlt@gmail.com>
2018-12-04 11:07:32 -0600

    se agregaron las pruebas de eficiencia

--- PruebaMetodosDeBusqueda.py
+++ PruebaMetodosDeBusqueda.py
@@ -5,6 +5,7 @@ Created on 29/11/2018
 '''
 import random
 import copy
+from time import time
 
 def ordenamientoPorInsercion(numeros):
     for i in range(1,len(numeros)):
@@ -17,31 +18,53 @@ def ordenamientoPorInsercion(numeros):
                 j=j-1
             else:
                 break
-
 def busquedaSecuencial(unaLista,datoBuscar):
+    comparaciones=0
+    recorridos=0
+    start_time = time()
     pos=0
     encontrado=False
-    
     while pos<len(unaLista)and not encontrado:
+        comparaciones=comparaciones+1
         if unaLista[pos]==datoBuscar:
             encontrado=True
         else:
             pos=pos+1
+        recorridos=recorridos+1
+    elapsed_time = time() - start_time
+    print("Tiempo de ejecucion: %.10f seconds." % elapsed_time)
+    print ("Recoridos: "+str (recorridos))
+    print ("Comparaciones: "+str (comparaciones))
     return encontrado
 
 def busquedaBinaria(unaLista,elemento):
     primero=0
     ultimo=len(unaLista)-1
+    comparaciones=0
+    recorridos=0
+    start_time = time()
     while(primero<=ultimo):
+        comparaciones=comparaciones+1
+        recorridos=recorridos+1
         centro=int ((primero+ultimo)/2)
         valorCentro=unaLista[centro]
         
         if(elemento==valorCentro):
+            comparaciones=comparaciones+1
+            elapsed_time = time() - start_time
+            print("Tiempo de ejecucion: %.10f seconds." % elapsed_time)
+            print ("Recoridos: "+str (recorridos))
+            print ("Comparaciones: "+str (comparaciones))
             return centro
         elif elemento<valorCentro:
+            comparaciones=comparaciones+1
             ultimo=centro-1
         else:
             primero=centro+1
+    elapsed_time = time() - start_time
+    print("Tiempo de ejecucion: %.10f seconds." % elapsed_time)
+    print ("Recoridos: "+str (recorridos))
+    print ("Comparaciones: "+str (comparaciones))
     return -1
 
 class has_table:
@@ -62,10 +85,9 @@ class has_table:
     def Search(self,value):
         hash=self.Hash_func(value)
         if self.table[hash] is None:
-            return None
+            return "No se encontro el elemento!"
         else:
-            print("Se encontro el elemento en")
-            return id(self.table[hash])
+            return "Se encontro el elemento!"
     
 
 menu=0
@@ -81,19 +103,27 @@ while(menu!=4):
     
     if menu==1:
         datoBuscar=int (input("Ingresa dato a buscar"))
-        print (busquedaSecuencial(copy.copy(arregloDesordenado), datoBuscar))
+        if(busquedaSecuencial(copy.copy(arregloDesordenado), datoBuscar)):
+            print("Dato encontrado")
+        else:
+            print("Dato no encontrado!")
     if menu==2:
         datoBuscar=int (input("Ingresa dato a buscar"))
         arr=copy.copy(arregloDesordenado)
         ordenamientoPorInsercion(arr)
-        a=busquedaBinaria(arr,datoBuscar)
-        if(a!=-1):
+        if(busquedaBinaria(arr,datoBuscar)!=-1):
             print("Dato encontrado!")
+        else:
+            print("Dato no encontrado!")
     if menu==3:
         H=has_table()
         arr=copy.copy(arregloDesordenado)
         for i in range (0,len(arr)):
             H.Insert(str(arr[i]))
         datoBuscar=str (input("Ingresa dato a buscar"))
+        start_time = time()
         print(H.Search(datoBuscar))
+        elapsed_time = time() - start_time
+        print("No hace recorridos ni comparaciones")
+        print("Tiempo de ejecucion: %.10f seconds." % elapsed_time)
     
